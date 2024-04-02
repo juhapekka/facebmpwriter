6c4a282616502d02fa7c173aef623536a6967bd3
Juha-Pekka Heikkilä <juhapekka.heikkila@gmail.com>
2024-04-02 22:17:14 +0300

    Set webcam to first camera to init

--- facewrite.py
+++ facewrite.py
@@ -6,7 +6,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # Initialize the webcam
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
